99c8cc212057809b5c0eb499fac54df4754a8eb3
joseph darko <osomfonashticah@gmail.com>
2024-01-30 22:11:36 +0000

    first commit

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author joseph darko <https://github.com/osomfonashticah>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
